9e12be3f00c59fabeacea8c41436dc82d393ebba
matubruno16 <matubruno16@gmail.com>
2024-03-16 09:45:01 -0300

    Update SpinIcons

--- src/app/components/SpinIconos.tsx
+++ src/app/components/SpinIconos.tsx
@@ -27,8 +27,8 @@ const generateImages = (angle: number): ImageData[] =>
   imageUrls.map((imageUrl, index) => ({
     url: imageUrl,
     position: {
-      left: `${50 + 40 * Math.cos((angle + 45 * index) * (Math.PI / 180))}%`,
-      top: `${50 + 40 * Math.sin((angle + 45 * index) * (Math.PI / 180))}%`,
+      left: `${40 + 40 * Math.cos((angle + 45 * index) * (Math.PI / 180))}%`,
+      top: `${40 + 40 * Math.sin((angle + 45 * index) * (Math.PI / 180))}%`,
     },
   }));
 
@@ -50,18 +50,28 @@ const SpinIconos = () => {
   const images = useMemo(() => generateImages(angle), [angle]);
 
   return (
-    <div className="relative w-[300px] h-[300px]" ref={circleRef}>
+    <div
+      className="relative flex items-center justify-center w-[400px] h-[400px]"
+      ref={circleRef}
+    >
+      <div className="absolute w-[337px] h-[337px] border-[20px] border-aro rounded-full flex justify-center items-center"></div>
       {images.map(({ url, position }) => (
         <Image
           key={url}
           src={url}
           alt="iconographic"
-          className=" rounded-full"
-          width={60}
-          height={60}
+          className="rounded-full"
+          width={70}
+          height={70}
           style={{ position: "absolute", ...position }}
         />
       ))}
+      <Image 
+        src="/crm.png"
+        alt="logo CRM"
+        width={180}
+        height={180}
+      />
     </div>
   );
 };
