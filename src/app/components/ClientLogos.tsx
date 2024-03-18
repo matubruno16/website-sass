572a0e3137a366407345c5d1e4afd6d65aaa9df9
matubruno16 <matubruno16@gmail.com>
2024-03-18 18:45:26 -0300

    Correccion posicionamiento de la  seccion

--- src/app/components/ClientLogos.tsx
+++ src/app/components/ClientLogos.tsx
@@ -5,12 +5,12 @@ import SliderClientes from "./SliderClientes";
 export default function ClientLogos() {
   const t = useTranslations("Home.ClientLogos");
   return (
-    <section className="w-full h-full mb-10 -translate-y-60">
+    <section className="w-full h-full -translate-y-40">
       <div className="flex flex-col items-center">
         <h2 className="text-4xl font-semibold text-text-primario">
           {t("title")}
         </h2>
-        <p className="text-text-secundario text-pretty w-1/4 mx-auto text-center mt-2">
+        <p className="text-text-secundario text-pretty w-2/4 mx-auto text-center mt-2">
           {t("subtitle")}
         </p>
       </div>
