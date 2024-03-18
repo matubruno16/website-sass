4ef438e976547a65be802b36a6c0e69b1000bed9
matubruno16 <matubruno16@gmail.com>
2024-03-18 17:20:22 -0300

    Creo Button

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,17 @@
+import { useTranslations } from "next-intl";
+import React from "react";
+
+interface Props {
+  text: string;
+}
+export default function Button({ text }: Props) {
+  const t = useTranslations("Home.Buttons");
+  return (
+    <a
+      href="#"
+      className="flex bg-btn w-[10rem] h-[2.75rem] rounded-md text-white justify-center items-center"
+    >
+      {t(`${text}`)}
+    </a>
+  );
+}
