797a47ae1cfb2a94a75caa3bcdc6e4e40c4fef60
matubruno16 <matubruno16@gmail.com>
2024-03-18 16:50:11 -0300

    Clientes slider

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,18 +1,17 @@
+import ClientLogos from "../components/ClientLogos";
 import CoreFeatures from "../components/CoreFeatures";
 import Features from "../components/Features";
 import Hero from "../components/Hero";
 import WhyChooseUs from "../components/WhyChooseUs";
 
 export default function Home() {
- 
-
   return (
     <main className="bg-background ">
       <Hero />
       <Features />
       <CoreFeatures />
       <WhyChooseUs />
-     
+      <ClientLogos />
     </main>
   );
 }
---
+++ src/app/components/SliderClientes.tsx
@@ -0,0 +1,169 @@
+
+import React from "react";
+
+export default function SliderClientes() {
+  return (
+    <div className="logos w-full overflow-hidden pt-16 bg-white whitespace-nowrap ">
+      <div className="logos-slider  overflow-hidden">
+        <img
+          loading="lazy"
+          src="/1.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/2.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/3.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/4.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/5.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/6.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/7.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/8.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/9.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/10.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/11.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/12.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/13.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+      </div>
+      <div className="logos-slider overflow-hidden">
+        <img
+          loading="lazy"
+          src="/1.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/2.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/3.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/4.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/5.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/6.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/7.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/8.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/9.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/10.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/11.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/12.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+        <img
+          loading="lazy"
+          src="/13.svg"
+          alt="Logo cliente"
+          className="px-10"
+        />
+      </div>
+    </div>
+  );
+}
