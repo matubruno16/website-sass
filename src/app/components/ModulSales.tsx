8f1858b5555ed93e620ee497fcb19698e1644867
matubruno16 <matubruno16@gmail.com>
2024-03-18 18:46:28 -0300

    Creo SalesModules y modulSales

---
+++ src/app/components/ModulSales.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface Props {
+    srcImg: string;
+    altImg: string;
+    title: string;
+    subtitle: string;
+
+}
+
+export default function ModulSales({ srcImg, altImg, title, subtitle } : Props) {
+  return (
+    <div className='flex w-[75%] border-b-2 border-text-text-secundario mx-auto  py-4'>
+        <Image 
+          src={srcImg} 
+          alt={altImg} 
+          width={40} 
+          height={40} 
+          loading='lazy'
+        />
+        <div className='pl-6'>
+        <a href='#' className='text-xl font-semibold text-text-primario'>{title}</a>
+        <p className='text-text-secundario'> {subtitle}</p>
+          
+        </div>
+    </div>
+  )
+}
--- src/app/components/SalesModules.tsx
+++ src/app/components/SalesModules.tsx
@@ -1,12 +1,100 @@
 import { useTranslations } from "next-intl";
 import React from "react";
+import Button from "./Button";
+import ModulSales from "./ModulSales";
 
 export default function SalesModules() {
   const t = useTranslations("Home.SalesModules");
   return (
-    <section>
-      <h2></h2>
-      <p></p>
+    <section className="w-full grid place-content-center">
+      <div className="flex flex-col items-center">
+        <h2 className="text-4xl font-semibold text-text-primario">
+          {t("title")}
+        </h2>
+        <p className="text-text-secundario text-pretty w-2/4 mx-auto text-center mt-2">
+          {t("subtitle")}
+        </p>
+      </div>
+      <div className="grid grid-cols-3 my-10 py-10 max-w-[1200px]  border-dashed border-2 border-aro rounded-[40px] mx-10 ">
+        <div className="flex flex-col w-full items-center ">
+          <Button text="btn-InNeed" />
+          <div className=" pt-8" >
+            <ModulSales
+              srcImg="/icon.svg"
+              altImg="Icono Descuento"
+              title="Sales"
+              subtitle="Unique and powerful suite sale run your entire business"
+            />
+            <ModulSales
+              srcImg="/icon-1.svg"
+              altImg="Icono Altavoz"
+              title="Marketing"
+              subtitle="Unique and powerful suite sale 
+              run your entire business"
+            />
+            <ModulSales
+              srcImg="/icon-2.svg"
+              altImg="Icono Service"
+              title="Service"
+              subtitle="Unique and powerful suite sale 
+              run your entire business"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col w-full items-center">
+          <Button text="btn-Industry" />
+          <div className=" pt-8">
+            <ModulSales
+              srcImg="/icon-3.svg"
+              altImg="Icono Automotive"
+              title="Automotive"
+              subtitle="Unique and powerful suite sale 
+              run your entire business"
+            />
+            <ModulSales
+              srcImg="/icon-4.svg"
+              altImg="Icono Real estate"
+              title="Real estate"
+              subtitle="Unique and powerful suite sale 
+              run your entire business"
+            />
+            <ModulSales
+              srcImg="/icon-5.svg"
+              altImg="Icono Education"
+              title="Education"
+              subtitle="Unique and powerful suite sale 
+              run your entire business"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col w-full items-center ">
+          <Button text="btn-Solution" />
+
+          <div className=" pt-8">
+            <ModulSales
+              srcImg="/icon-6.svg"
+              altImg="Icono Project Management"
+              title="Project Management"
+              subtitle="Unique and powerful suite sale 
+              run your entire business"
+            />
+            <ModulSales
+              srcImg="/icon-7.svg"
+              altImg="Icono Delivery Management"
+              title="Delivery Management"
+              subtitle="Unique and powerful suite sale 
+              run your entire business"
+            />
+            <ModulSales
+              srcImg="/icon-8.svg"
+              altImg="Icono Loyalty Management"
+              title="Loyalty Management"
+              subtitle="Unique and powerful suite sale 
+              run your entire business"
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
