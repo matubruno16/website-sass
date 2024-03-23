0a683c36ba5d02f4d835eafdf0dfa1d3ace3da50
matubruno16 <matubruno16@gmail.com>
2024-03-23 10:15:23 -0300

    Correccion semantica

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,13 +7,13 @@ import WhyChooseUs from "../components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="bg-background ">
+    <>
       <Hero />
       <Features />
       <CoreFeatures />
       <WhyChooseUs />
       <ClientLogos />
       <SalesModules />
-    </main>
+    </>
   );
 }
