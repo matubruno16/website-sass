ebb1c1711fff0f98928917b32ec67bc9d755a25b
matubruno16 <matubruno16@gmail.com>
2025-02-27 09:52:39 -0300

    Update i18n

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,15 +1,9 @@
-import {defineRouting} from 'next-intl/routing';
-import {createNavigation} from 'next-intl/navigation';
- 
+import { defineRouting } from "next-intl/routing";
+import { createNavigation } from "next-intl/navigation";
+
 export const routing = defineRouting({
-  // A list of all locales that are supported
-  locales: ['en', 'es', 'pt'],
- 
-  // Used when no locale matches
-  defaultLocale: 'en'
+  locales: ["en", "es", "pt"], 
+  defaultLocale: "en", 
 });
- 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
-export const {Link, redirect, usePathname, useRouter, getPathname} =
+export const { Link, redirect, usePathname, useRouter } =
   createNavigation(routing);
