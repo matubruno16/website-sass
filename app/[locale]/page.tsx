8144197ba7a322e93a83479dd507521a693fc9b9
matubruno16 <matubruno16@gmail.com>
2025-03-05 18:23:58 -0300

    Secction Features

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import Features from '../components/Features';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
     <Header />
     <Hero />
+    <Features />
     </>
   );
 }
