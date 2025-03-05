cfc360d9bb6f9d8c5c62d32e5e1f8c0acfaecd7c
matubruno16 <matubruno16@gmail.com>
2025-03-05 18:11:29 -0300

    Update Hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,33 +2,29 @@ import React from "react";
 import Image from "next/image";
 import { getTranslations } from "next-intl/server";
 import Button from "./Button";
-
 export default async function Hero() {
   const t = await getTranslations("Home.Hero");
   return (
     <section className="bg-heading h-full overflow-hidden">
-      <div className="max-w-[1440px] mx-auto h-full flex flex-col md:ml-[5%] lg:ml-40 mt-40 overflow-hidden text-center md:text-start px-10 md:px-0">
+      <div className="max-w-[1440px] mx-auto h-full flex flex-col md:ml-[5%] lg:ml-40 mt-40 overflow-hidden text-center md:text-start px-2 md:px-0">
         <h1 className=" text-5xl  md:text-6xl font-semibold text-pretty leading-[3.8rem] md:w-[590px] ">
           {t("title")}
         </h1>
         <div className="text-2xl my-4 md:w-[590px]">
           <p className="text-balance">{t("subtitle")}</p>
         </div>
-        <div className="w-full flex flex-col md:flex-row items-center justify-center md:justify-start gap-6 text-lg font-semibold mt-6 mb-20 ">
-          <Button url="/" text="btn-hero" />
-          <a href="#" className=" ">
-            {t("link")}
-          </a>
+        <div className="w-full flex flex-col md:flex-row items-center justify-center md:justify-start gap-6 text-lg font-semibold mt-6 md:mb-20 ">
+          <Button text={t("link")} url="/contact" />
+        <Image
+          className=" md:ml-40"
+          src="/star5.png"
+          alt="Certificacion 5 estrellas"
+          width={300}
+          height={200}
+          loading="lazy"
+        />
         </div>
       </div>
-      <Image
-        className="ml-[5%] md:ml-40"
-        src="/star5.png"
-        alt="Certificacion 5 estrellas"
-        width={1200}
-        height={400}
-        loading="lazy"
-      />
     </section>
   );
 }
